fa7535ce8fe378aea6ccedbba7e92cf3e8bce442
BenTelega <72995848+BenTelega@users.noreply.github.com>
2024-11-21 09:03:52 +0000

    fix

--- bot.py
+++ bot.py
@@ -17,19 +17,28 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start(message: types.Message):
+    """
+    Обрабатывает нажатие start.
+    """
+    await message.answer(text='Привет!')
+
+
+@dp.message(F.text == '/wa')
+async def cmd_back_home(message: types.Message) -> None:
+
     webAppInfo = types.WebAppInfo(
         url="https://probable-capybara-7wqwp99x57gcpv5r-5500.app.github.dev")
     builder = ReplyKeyboardBuilder()
     builder.add(types.KeyboardButton(
         text='Отправить данные', web_app=webAppInfo))
 
-    await message.answer(text='Привет!', reply_markup=builder.as_markup())
+    await message.answer(text='Тестирование веб приложения', reply_markup=builder.as_markup(resize_keyboard=True))
 
 
 @dp.message(F.content_type == ContentType.WEB_APP_DATA)
 async def parse_data(message: types.Message):
     data = json.loads(message.web_app_data.data)
-    await message.answer(f'<b>{data["title"]}</b>\n\n<code>{data["desc"]}</code>\n\n{data["text"]}', parse_mode=ParseMode.HTML)
+    await message.answer(f'<b>{data["title"]}</b>\n\n{data["text"]}', parse_mode=ParseMode.HTML)
 
 
 async def main():
