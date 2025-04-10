6302493aa8ca959cacd4acc59ba78b04cecac1b4
BenTelega <72995848+BenTelega@users.noreply.github.com>
2025-04-10 23:07:09 +0000

    fast deploy

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import os
 
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.enums.content_type import ContentType
 from aiogram.filters import CommandStart
 from aiogram.enums.parse_mode import ParseMode
@@ -20,7 +21,26 @@ async def start(message: types.Message):
     """
     Обрабатывает нажатие start.
     """
-    await message.answer(text='Привет!')
+    # webAppInfo = types.WebAppInfo(
+    #     url="https://bentelega.ru")
+    # builder = ReplyKeyboardBuilder()
+    # builder.add(types.InlineKeyboardButton(
+    #     text='Смотреть', web_app=webAppInfo))
+
+    webAppInfo = types.WebAppInfo(
+        url="https://bentelega.ru")
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(
+                text="Канал", url="https://t.me/bentelega_channel"), InlineKeyboardButton(
+                text="Написать", url="https://t.me/bentelega")],
+            [InlineKeyboardButton(
+                text="Посмотреть", web_app=webAppInfo)],
+        ]
+    )
+
+    await message.answer(text='<b>Решения для Telegram</b>\n\n<code>Готовые к внедрению веб-приложения и боты для вашего бизнеса</code>', reply_markup=keyboard,  parse_mode="HTML")
 
 
 @dp.message(F.text == '/wa')
@@ -30,7 +50,7 @@ async def cmd_back_home(message: types.Message) -> None:
         url="https://bentelega.ru")
     builder = ReplyKeyboardBuilder()
     builder.add(types.KeyboardButton(
-        text='Отправить данные', web_app=webAppInfo))
+        text='Смотреть', web_app=webAppInfo))
 
     await message.answer(text='Тестирование веб приложения', reply_markup=builder.as_markup(resize_keyboard=True))
 
