b9db4e7daeea68e4162db478dc378e2c954fda09
benj <reload.benjamin@gmail.com>
2025-05-02 03:11:48 +0300

    fast deploy

---
+++ tailwind-config.js
@@ -0,0 +1,33 @@
+// tailwind-config.js
+window.tailwindConfig = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: "#c44569",
+          dark: "#546de5",
+        },
+        primaryDark: {
+          light: "#006699",
+          dark: "#0077cc",
+        },
+        secondary: {
+          light: "#f8f9fa",
+          dark: "#303952",
+        },
+        dark: {
+          light: "#212529",
+          dark: "#e1e1e1",
+        },
+        light: {
+          light: "#ffffff",
+          dark: "#2d2d2d",
+        },
+      },
+      fontFamily: {
+        "roboto-slab": ["Roboto Slab", "serif"],
+      },
+    },
+  },
+};
